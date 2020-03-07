6433f7621766c02ff942d76a0787589c3ce71aa2
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-03-07 09:54:47 -0300

    Update codes comments

--- src/det.hpp
+++ src/det.hpp
@@ -3,10 +3,28 @@
 
 namespace Laplace
 {
+    /**
+     * Calculates determinant of a 2x2 matrix
+     * 
+     * @param arr Container of values
+     * @return Determinant of a 2x2 matrix
+     */
     double Determinant2D(const xt::xarray<double> arr);
     
+    /**
+     * Calculates determinant of a 3x3 matrix
+     * 
+     * @param arr Container of values
+     * @return Determinant of a 3x3 matrix
+     */
     double Determinant3D(const xt::xarray<double> arr);
     
+    /**
+     * Calculates determinant of a NxN matrix
+     * 
+     * @param arr Container of values
+     * @return Determinant of a NxN matrix
+     */
     double Determinant(const xt::xarray<double> arr);
 }
 
--- src/dimension.hpp
+++ src/dimension.hpp
@@ -3,8 +3,21 @@
 
 namespace Laplace
 {
+    /**
+     * Verify if the input matrix is square (NxM, where N = M)
+     * 
+     * @param arr Container of matrix
+     * @return Flag indicating whether the matrix is square
+     */
     bool IsMatrixSquare(const xt::xarray<double> arr);
 
+    /**
+     * Utilitary function to verify the dimensions of matrixs
+     * 
+     * @param arr Container of matrix
+     * @param dimension Dimension verified in matrix
+     * @return Flag indicating whether the matrix dimension is equals inserted dimension
+     */
     bool CheckDimension(const xt::xarray<double> arr, const unsigned int dimension);
 }
 
